6a0a84d009cc93c5db7b0cf5923260e6b1333810
Miha F <miha.fabcic@soniox.com>
2026-01-28 16:51:30 +0100

    Remove get_temporary_api_key from realtime
    - if user needs this, he can use client.auth... function

--- src/soniox/realtime/__init__.py
+++ src/soniox/realtime/__init__.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from functools import cached_property
 from typing import TYPE_CHECKING
 
-from ..types import CreateTemporaryApiKeyPayload
 from ..types.realtime import RealtimeEvent, RealtimeSttConfig, RealtimeToken
 from .stt import RealtimeSTTClient, RealtimeSTTSession
 
@@ -39,9 +38,6 @@ class AsyncRealtimeAPI:
         self._stt = AsyncRealtimeSTTClient(self._client)
         return self._stt
 
-    async def get_temporary_api_key(self, payload: CreateTemporaryApiKeyPayload):
-        return await self._client.auth.create_temporary_api_key(payload)
-
 
 class RealtimeAPI:
     def __init__(self, client: SonioxClient) -> None:
@@ -53,6 +49,3 @@ class RealtimeAPI:
 
         self._stt = RealtimeSTTClient(self._client)
         return self._stt
-
-    def get_temporary_api_key(self, payload: CreateTemporaryApiKeyPayload):
-        return self._client.auth.create_temporary_api_key(payload)
